1a34ecf317cfb80747a28a658fc9bb9969164776
MaksimKochetkov <MaksimKochetkov8351@gmail.com>
2023-09-20 17:39:18 +0500

    dfsds

--- app/src/main/java/com/example/feeling_diary/Hi_screen.kt
+++ app/src/main/java/com/example/feeling_diary/Hi_screen.kt
@@ -3,16 +3,31 @@ package com.example.feeling_diary
 import android.R.attr.value
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.feeling_diary.R.id.edittextemail
+import com.example.feeling_diary.R.id.edittextpass
+import java.util.regex.Pattern
 
 
 class Hi_screen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hi_screen)
+
+    }
+
+    fun go_to_recycle(view: View) {
         val myIntent = Intent(this@Hi_screen, main_screen::class.java)
         myIntent.putExtra("go_to_recycle", value)
+        this@Hi_screen.startActivity(myIntent)}
 
-        this@Hi_screen.startActivity(myIntent)
-    }
+    fun go_to_sign(view: View) {
+    val my1Intent = Intent(this@Hi_screen, signin::class.java)
+    my1Intent.putExtra("go_to_sign", value)
+    this@Hi_screen.startActivity(my1Intent)}
 }
--- app/src/main/java/com/example/feeling_diary/signin.kt
+++ app/src/main/java/com/example/feeling_diary/signin.kt
@@ -1,11 +1,46 @@
 package com.example.feeling_diary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import java.util.regex.Pattern
 
 class signin : AppCompatActivity() {
+    val pattern = ("[a-z]{1,100}" + "@" + "[a-z]{1,6}" + "\\." + "[a-z]{1,3}")
+    lateinit var email: EditText
+    lateinit var pass: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+        email = findViewById(R.id.edittextemail)
+        pass = findViewById(R.id.edittextpass)
+    }
+    fun emailValid(text:String):Boolean
+    {
+        return Pattern.compile(pattern).matcher(text).matches()
+    }
+    fun but(view: View) {
+        if (email.text.toString().isEmpty()||pass.text.toString().isEmpty()) {
+            Toast.makeText(this, "Что-то пусто", Toast.LENGTH_SHORT).show()
+        }
+        if (email.text.toString().isNotEmpty()&&pass.text.toString().isNotEmpty())
+        {
+            if (emailValid(email.text.toString()))
+            {
+                val intent = Intent(this@signin, main_screen::class.java)
+                startActivity(intent)
+                finish()
+            }
+            else {
+                Toast.makeText(this, "Ошибка", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else {
+            val alert = AlertDialog.Builder(this)
+        }
     }
 }
